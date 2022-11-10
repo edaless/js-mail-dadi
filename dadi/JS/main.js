052cb428f295c3da7f945fd08b9f2b97cfeae868
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-10 16:08:35 +0100

    fine

--- dadi/JS/main.js
+++ dadi/JS/main.js
@@ -4,17 +4,43 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
+
+
+// la case serve solo per il colore
+const caseUser = document.querySelector(".user");
+
+// assegno numero random a una costante
 const numUser = Math.floor(Math.random() * 6) + 1;
+// lo stampo
 console.log( "User: " + numUser);
+// lo mando all'id nell'html
+document.getElementById("numUser").innerHTML = numUser;
+
+
+const casePc = document.querySelector(".pc");
 const numPc = Math.floor(Math.random() * 6) + 1;
 console.log( "Pc: " + numPc);
+document.getElementById("numPc").innerHTML = numPc;
+
 
 
 if(numPc > numUser){
+    document.getElementById("frecciatina").innerHTML = "Hai perso miseramente";
     console.log("Mi dispiace ma hai perso miseramente");
+
+    
+    caseUser.style.backgroundColor = "red";
+    casePc.style.backgroundColor = "green";
+
 }else if(numPc < numUser){
+    document.getElementById("frecciatina").innerHTML = "Hai vinto";
     console.log("Hai vinto!!!");
+
+
+    caseUser.style.backgroundColor = "green";
+    casePc.style.backgroundColor = "red";
 }else{
+    document.getElementById("frecciatina").innerHTML = "Parità";
     console.log("Parità");
 };
 
